9aeb66cc4d146c296caf507436599b5c60150362
tnstjddlrl <48933775+tnstjddlrl@users.noreply.github.com>
2021-11-20 20:16:13 +0900

    병합 전 커밋

--- App.js
+++ App.js
@@ -43,6 +43,7 @@ const App = () => {
 
           {/* <Stack.Screen name="로그인 페이지" component={LoginPage} /> */}
           {/* <Stack.Screen name="회원가입 페이지" component={RegisterPage} /> */}
+          <Stack.Screen name="매장예약 페이지" component={storeregisterPage} />
           <Stack.Screen name="메인페이지" component={mainPage} />
 
           <Stack.Screen name="실시간 예약 페이지" component={CurrentReservation} />
@@ -55,7 +56,6 @@ const App = () => {
           {/* <Stack.Screen name="메뉴 등록 페이지" component={menuenrollmentPage} /> */}
           {/* <Stack.Screen name="메뉴 수정 페이지" component={menurevisePage} /> */}
           {/* <Stack.Screen name="메뉴 삭제 페이지" component={menudeletePage} /> */}
-          {/* <Stack.Screen name="매장예약 페이지" component={storeregisterPage} /> */}
           {/* <Stack.Screen name="매장사장상세페이지" component={M_storedetailPage} /> */}
           {/* <Stack.Screen name="매장둘러보기페이지" component={storelookPage} /> */}
           {/* <Stack.Screen name="테스트 페이지" component={test} /> */}
