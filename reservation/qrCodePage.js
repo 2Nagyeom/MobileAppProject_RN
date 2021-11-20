ae95404e9923aec9b7564c692b799a5d0d1417e2
2eunbi <70017526+2eunbi@users.noreply.github.com>
2021-11-20 20:54:51 +0900

    qr코드 생성성공

--- reservation/qrCodePage.js
+++ reservation/qrCodePage.js
@@ -1,19 +1,53 @@
 
-
+import React from 'react';
 import QRCode from 'react-native-qrcode-svg';
+import {
+    SafeAreaView,
+    ScrollView,
+    StyleSheet,
+    Text,
+    TextInput,
+    View,
+    Image,
+    TouchableWithoutFeedback,
+    TouchableOpacity,
+    Dimensions,
+    Alert
+} from 'react-native';
+
+import { useNavigation } from '@react-navigation/core';
+
 
 
 const qrcodePage = () => {
-    <View style={styles.qrCodeStyle}>
-        <View>
-            <QRCode
-                size={200}			  // 로고 사이즈 조절
-                value={data.address}   // 실제 연결 될 주소 
-                logoSize={300}
-                logoBackgroundColor='transparent'
-            />
+
+    const navigation = useNavigation();
+
+    return (
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+            <View style={{
+                marginBottom: 10,
+            }}>
+                <Text style={{
+                    fontSize: 20,
+                }}>
+                    입장 QR코드 입니다!
+                </Text>
+            </View>
+            <QRCode value="test/now/now/01012341234" />
+            <TouchableWithoutFeedback onPress={() => {
+                navigation.navigate('메인페이지');
+            }}>
+                <View style={{
+                    marginTop: 40,
+                }}>
+                    <Text style={{ fontSize: 15 }}>
+                        뒤로가기
+                    </Text>
+                </View>
+            </TouchableWithoutFeedback>
         </View>
-    </View>
+    )
 }
 
 export default qrcodePage;
