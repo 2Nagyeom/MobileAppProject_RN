2997d322273bfd0599ad80e6b7cff6fb171ee795
2eunbi <70017526+2eunbi@users.noreply.github.com>
2021-11-20 16:24:08 +0900

    개인정보수정페이지 및 네비게이션

--- App.js
+++ App.js
@@ -21,6 +21,7 @@ import mainPage from './signs/mainPage';
 import M_storedetailPage from './signs/m_storedetailPage';
 import test from './signs/test';
 import storelookPage from './signs/storelookPage';
+import imformationrevisePage from './signs/imformationrevisePage';
 
 import { NavigationContainer } from '@react-navigation/native'; //화면을 쌓아주는 레이어!
 import { createNativeStackNavigator } from '@react-navigation/native-stack'; //레이어 위에 올라가는 화면들!
@@ -46,9 +47,10 @@ const App = () => {
         {/* <Stack.Screen name="메뉴 수정 페이지" component={menurevisePage} /> */}
         {/* <Stack.Screen name="메뉴 삭제 페이지" component={menudeletePage} /> */}
         {/* <Stack.Screen name="매장예약 페이지" component={storeregisterPage} /> */}
-        <Stack.Screen name="메인페이지" component={mainPage} />
+        {/* <Stack.Screen name="메인페이지" component={mainPage} /> */}
         {/* <Stack.Screen name="매장사장상세페이지" component={M_storedetailPage} /> */}
-        <Stack.Screen name="매장둘러보기페이지" component={storelookPage} />
+        {/* <Stack.Screen name="매장둘러보기페이지" component={storelookPage} /> */}
+        <Stack.Screen name="메뉴 등록 페이지" component={imformationrevisePage} />
         {/* <Stack.Screen name="테스트 페이지" component={test} /> */}
 
 
