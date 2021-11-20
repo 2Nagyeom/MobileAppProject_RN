cd20ddf565308ccb6d54ede76948fec5e50f8312
tnstjddlrl <48933775+tnstjddlrl@users.noreply.github.com>
2021-11-20 13:18:25 +0900

    안드로이드 바텀시트 업데이트

--- App.js
+++ App.js
@@ -44,9 +44,9 @@ const App = () => {
         {/* <Stack.Screen name="예약자확인 페이지" component={bookerPage} /> */}
         {/* <Stack.Screen name="메뉴 등록 페이지" component={menuenrollmentPage} /> */}
         {/* <Stack.Screen name="메뉴 수정 페이지" component={menurevisePage} /> */}
-        <Stack.Screen name="메뉴 삭제 페이지" component={menudeletePage} />
+        {/* <Stack.Screen name="메뉴 삭제 페이지" component={menudeletePage} /> */}
         {/* <Stack.Screen name="매장예약 페이지" component={storeregisterPage} /> */}
-        {/* <Stack.Screen name="메인페이지" component={mainPage} /> */}
+        <Stack.Screen name="메인페이지" component={mainPage} />
         {/* <Stack.Screen name="매장사장상세페이지" component={M_storedetailPage} /> */}
         {/* <Stack.Screen name="매장둘러보기페이지" component={storelookPage} /> */}
         {/* <Stack.Screen name="테스트 페이지" component={test} /> */}
--- signs/storeregisterPage.js
+++ signs/storeregisterPage.js
@@ -26,6 +26,9 @@ LocaleConfig.locales['fr'] = {
 LocaleConfig.defaultLocale = 'fr';
 
 const storeregisterPage = () => {
+    var dt = new Date();
+
+
     return (
         <SafeAreaView style={{ backgroundColor: 'white', flex: 1 }}>
             <View style={{
@@ -46,59 +49,36 @@ const storeregisterPage = () => {
                 backgroundColor: 'white',
             }}>
                 <Calendar
-                    // Initially visible month. Default = Date()
-                    current={'2021-01-01'}
-                    // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
-                    minDate={'2021-11-01'}
-                    // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
-                    maxDate={'2021-12-31'}
-                    // Handler which gets executed on day press. Default = undefined
+                    current={dt.getFullYear() + '-' + (dt.getMonth() + 1) + '-' + dt.getDate()}
+                    minDate={dt.getFullYear() + '-' + (dt.getMonth() + 1) + '-' + dt.getDate()}
+                    maxDate={dt.getFullYear() + '-' + (dt.getMonth() + 1) + '-' + (dt.getDate() + 7)}
                     onDayPress={(day) => { console.log('selected day', day) }}
-                    // Handler which gets executed on day long press. Default = undefined
                     onDayLongPress={(day) => { console.log('selected day', day) }}
-                    // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
                     monthFormat={'yyyy MM'}
-                    // Handler which gets executed when visible month changes in calendar. Default = undefined
                     onMonthChange={(month) => { console.log('month changed', month) }}
-                    // Hide month navigation arrows. Default = false
                     hideArrows={true}
-                    // Replace default arrows with custom ones (direction can be 'left' or 'right')
                     renderArrow={(direction) => (<Arrow />)}
-                    // Do not show days of other months in month page. Default = false
                     hideExtraDays={false}
-                    // If hideArrows = false and hideExtraDays = false do not switch month when tapping on greyed out
-                    // day from another month that is visible in calendar page. Default = false
                     disableMonthChange={true}
-                    // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday
                     firstDay={1}
-                    // Hide day names. Default = false
                     hideDayNames={false}
-                    // Show week numbers to the left. Default = false
                     showWeekNumbers={false}
-                    // Handler which gets executed when press arrow icon left. It receive a callback can go back month
                     onPressArrowLeft={subtractMonth => subtractMonth()}
-                    // Handler which gets executed when press arrow icon right. It receive a callback can go next month
                     onPressArrowRight={addMonth => addMonth()}
-                    // Disable left arrow. Default = false
                     disableArrowLeft={true}
-                    // Disable right arrow. Default = false
                     disableArrowRight={true}
-                    // Disable all touch events for disabled days. can be override with disableTouchEvent in markedDates
                     disableAllTouchEventsForDisabledDays={true}
-                    // Replace default month and year title with custom one. the function receive a date as parameter
-                    // renderHeader={(date) => {/*Return JSX*/ }}
-                    // Enable the option to swipe between months. Default = false
                     enableSwipeMonths={true}
 
                     markingType={'period'}
                     markedDates={{
-                        '2021-11-11': { startingDay: true, color: '#50cebb', textColor: 'white' },
-                        '2021-11-12': { color: '#70d7c7', textColor: 'white' },
-                        '2021-11-13': { color: '#70d7c7', textColor: 'white' },
-                        '2021-11-14': { color: '#70d7c7', textColor: 'white' },
-                        '2021-11-15': { color: '#70d7c7', textColor: 'white', marked: true, dotColor: 'white' },
-                        '2021-11-16': { color: '#70d7c7', textColor: 'white' },
-                        '2021-11-17': { endingDay: true, color: '#50cebb', textColor: 'white' }
+                        '2021-11-19': { startingDay: true, color: '#50cebb', textColor: 'white' },
+                        '2021-11-20': { color: '#70d7c7', textColor: 'white' },
+                        '2021-11-21': { color: '#70d7c7', textColor: 'white' },
+                        '2021-11-22': { color: '#70d7c7', textColor: 'white' },
+                        '2021-11-23': { color: '#70d7c7', textColor: 'white', marked: true, dotColor: 'white' },
+                        '2021-11-24': { color: '#70d7c7', textColor: 'white' },
+                        '2021-11-25': { endingDay: true, color: '#50cebb', textColor: 'white' }
                     }}
                 />
             </View>
