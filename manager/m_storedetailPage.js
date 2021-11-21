b52eee977f8c8a6ed4e12782c7fa1e5ed3181205
tnstjddlrl <48933775+tnstjddlrl@users.noreply.github.com>
2021-11-21 22:20:58 +0900

    예약확인 페이지 수정완료

--- manager/m_storedetailPage.js
+++ manager/m_storedetailPage.js
@@ -33,113 +33,107 @@ const m_storedetailPage = () => {
     return (
 
         <SafeAreaView style={{ height: '100%', width: '100%', backgroundColor: '#F7F7F7' }}>
-
-            <View>
-                <View style={{
-                    marginTop: 10,
-                    marginBottom: 50,
-                    alignItems: 'center'
-                }}>
-                    <View>
-                        <AutoHeightImage
-                            width={150}
-                            source={Logo}
-                        />
-                    </View>
+            <View style={{
+                marginTop: 10,
+                marginBottom: 50,
+                alignItems: 'center'
+            }}>
+                <View>
+                    <AutoHeightImage
+                        width={150}
+                        source={Logo} />
                 </View>
-                <View
-                    style={{
-                        width: chwidth - 70,
-                        marginLeft: 20,
-                        marginTop: 10,
-                        alignItems: 'center',
-                    }}>
-                    <Text style={{
-                        color: '#6485E6',
-                        fontSize: 20,
-                    }}>당일예약자가 있으니 확인해주세요!</Text>
-                </View>
-                <View style={{
-                    borderWidth: 1,
-                    marginLeft: 20,
-                    width: chwidth - 40,
-                    height: 0,
-                    borderColor: '#6485E6',
-                }}>
-                    <View
-                        style={{
-                            marginTop: 10,
-                            borderRadius: 60,
-                            borderWidth: 1,
-                            width: chwidth - 40,
-                            height: 50,
-                            alignItems: 'center',
-                            justifyContent: 'center',
-                            backgroundColor: '#6485E6',
-                            borderColor: '#6485E6',
-                        }}>
-                        <Text style={{
-                            color: 'black',
-                            fontWeight: '700'
-                        }}>예약자 확인하기</Text>
-                    </View>
-                </View>
-
-                <View
-                    style={{
-                        width: chwidth - 70,
-                        marginLeft: 20,
-                        marginTop: 200,
-                        alignItems: 'center',
-                    }}>
-                    <Text style={{
-                        color: '#6485E6',
-                        fontSize: 20,
-                    }}>다른 날에 예약한 사람이 있어요!</Text>
-                </View>
-                <View style={{
-                    borderWidth: 1,
-                    width: chwidth - 40,
-                    height: 0,
-                    borderColor: '#6485E6',
-                    marginLeft: 20,
-
-                }}>
-                    <View
-                        style={{
-                            marginTop: 10,
-                            borderRadius: 60,
-                            borderWidth: 1,
-                            width: chwidth - 40,
-                            height: 50,
-                            alignItems: 'center',
-                            justifyContent: 'center',
-                            backgroundColor: '#6485E6',
-                            borderColor: '#6485E6',
-                        }}>
-                        <Text style={{ color: 'black', fontWeight: '700' }}>예약자 확인하기</Text>
-                    </View>
-                </View>
-
-                <TouchableWithoutFeedback onPress={() => navigation.navigate("매장사장 메인페이지")}>
-                    <View style={{ marginTop: 20 }}
-                        style={{
-                            marginLeft: 20,
-                            marginTop: 200,
-                            borderRadius: 60,
-                            borderWidth: 1,
-                            width: chwidth - 40,
-                            height: 50,
-                            alignItems: 'center',
-                            justifyContent: 'center',
-                            borderColor: '#6485E6',
-                        }}>
-                        <Text style={{ color: 'black' }}>뒤로가기</Text>
-                    </View>
-                </TouchableWithoutFeedback>
             </View>
+            <View style={{
+                width: chwidth - 70,
+                marginLeft: 30,
+                marginTop: 10,
+            }}>
+                <Text style={{
+                    color: '#6485E6',
+                    fontSize: 20,
+                }}>당일예약자</Text>
+            </View>
+            <View style={{
+                borderWidth: 1,
+                marginLeft: 20,
+                width: chwidth - 40,
+                height: 0,
+                borderColor: '#6485E6',
+            }}>
+            </View>
+            <View style={{ flex: 1, marginBottom: 10 }}>
+                <ScrollView style={{ flex: 1 }}>
+
+
+                    <ScrollViewItem></ScrollViewItem>
+                    <ScrollViewItem></ScrollViewItem>
+                    <ScrollViewItem></ScrollViewItem>
+
+                </ScrollView>
+            </View>
+
+
+
+            <View style={{
+                width: chwidth - 70,
+                marginLeft: 20,
+                alignItems: 'center',
+            }}>
+                <Text style={{
+                    color: '#6485E6',
+                    fontSize: 20,
+                }}>다른 날에 예약한 사람이 있어요!</Text>
+            </View>
+            <View style={{
+                borderWidth: 1,
+                width: chwidth - 40,
+                height: 0,
+                borderColor: '#6485E6',
+                marginLeft: 20,
+            }}>
+            </View>
+
+            <View style={{ flex: 1, marginBottom: 10 }}>
+                <ScrollView style={{ flex: 1 }}>
+                    <ScrollViewItem></ScrollViewItem>
+                    <ScrollViewItem></ScrollViewItem>
+                    <ScrollViewItem></ScrollViewItem>
+                </ScrollView>
+            </View>
+
+            <TouchableWithoutFeedback onPress={() => navigation.navigate("매장사장 메인페이지")}>
+                <View style={{
+                    marginLeft: 20,
+                    borderRadius: 60,
+                    borderWidth: 1,
+                    width: chwidth - 40,
+                    height: 50,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    borderColor: '#6485E6',
+                    marginBottom: 10
+                }}>
+                    <Text style={{ color: 'black' }}>뒤로가기</Text>
+                </View>
+            </TouchableWithoutFeedback>
         </SafeAreaView >
     );
 };
+
+const ScrollViewItem = () => {
+    return (
+        <View>
+            <View style={{ width: chwidth - 60, marginLeft: 30, marginTop: 10, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+                <Text>아이디</Text>
+                <Text>날짜/시간</Text>
+                <View style={{ borderRadius: 10, width: 50, height: 40, backgroundColor: 'orange', alignItems: 'center', justifyContent: 'center' }}>
+                    <Text>삭제</Text>
+                </View>
+            </View>
+            <View style={{ width: chwidth - 60, borderWidth: 0.5, marginLeft: 30, marginTop: 5 }}></View>
+        </View>
+    )
+}
 
 export default m_storedetailPage;
